0e84e7243ea620807fe19ff197f09eac28ec66d1
どらすず <dorasuzu58@gmail.com>
2024-01-05 19:13:39 +0900

    update EventBus

--- src/main/java/com/dorasuzublog/simpleoreexpansion/SimpleOreExpansion.java
+++ src/main/java/com/dorasuzublog/simpleoreexpansion/SimpleOreExpansion.java
@@ -1,10 +1,20 @@
 package com.dorasuzublog.simpleoreexpansion;
 
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.RegisterEvent;
+import net.neoforged.neoforge.registries.RegisterEvent.RegisterHelper;
 
 @Mod(SimpleOreExpansion.MODID)
 public class SimpleOreExpansion {
     public static final String MODID = "simpleoreexpansion";
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ModEventsSubscriber {
+        
+        @SubscribeEvent
+        public void onEvent() {
+            
+        }
+    }
 }
