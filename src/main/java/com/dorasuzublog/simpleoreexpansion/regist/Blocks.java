8c5527a29a43dfd06008cd2589b4befc4ec48ce0
どらすず <dorasuzu58@gmail.com>
2024-01-05 19:10:20 +0900

    Add regist folder

---
+++ src/main/java/com/dorasuzublog/simpleoreexpansion/regist/Blocks.java
@@ -0,0 +1,11 @@
+package com.dorasuzublog.simpleoreexpansion.regist;
+
+import net.minecraft.util.datafix.fixes.References;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+public class Blocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(NeoForgeRegistries.BLOCKS, "simpleoreexpansion");
+    
+}
---
+++ src/main/java/com/dorasuzublog/simpleoreexpansion/regist/Items.java
@@ -0,0 +1,11 @@
+package com.dorasuzublog.simpleoreexpansion.regist;
+
+import net.minecraft.util.datafix.fixes.References;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+public class Items {
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(NeoForgeRegistries.ITEMS, "simpleoreexpansion");
+    
+}
