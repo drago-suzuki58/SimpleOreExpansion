84f5489b4e2815c2e090cdfc02a0bd40bcd51971
どらすず <dorasuzu58@gmail.com>
2024-01-06 02:17:54 +0900

    Fix mod not loading problem

--- src/main/java/com/dorasuzublog/simpleoreexpansion/SimpleOreExpansion.java
+++ src/main/java/com/dorasuzublog/simpleoreexpansion/SimpleOreExpansion.java
@@ -3,16 +3,7 @@ package com.dorasuzublog.simpleoreexpansion;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 
-@Mod(SimpleOreExpansion.MODID)
+@Mod("simpleoreexpansion")
 public class SimpleOreExpansion {
     public static final String MODID = "simpleoreexpansion";
-
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ModEventsSubscriber {
-        
-        @SubscribeEvent
-        public void onEvent() {
-            
-        }
-    }
 }
