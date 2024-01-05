5980b2c3b82cbbd09cad700ed3fc193dcc4137cd
どらすず <dorasuzu58@gmail.com>
2024-01-05 15:53:20 +0900

    Add CreativeTab.java

--- src/main/java/com/dorasuzublog/simpleoreexpansion/SimpleOreExpansion.java
+++ src/main/java/com/dorasuzublog/simpleoreexpansion/SimpleOreExpansion.java
@@ -1,4 +1,10 @@
 package com.dorasuzublog.simpleoreexpansion;
 
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+@Mod(SimpleOreExpansion.MODID)
 public class SimpleOreExpansion {
+    public static final String MODID = "simpleoreexpansion";
 }
