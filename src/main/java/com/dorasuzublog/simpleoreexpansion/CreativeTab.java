d2f49417af277337e9aad00a5c069cf98fce8b9b
どらすず <dorasuzu58@gmail.com>
2024-01-07 01:43:27 +0900

    Update CreativeTab

--- src/main/java/com/dorasuzublog/simpleoreexpansion/CreativeTab.java
+++ src/main/java/com/dorasuzublog/simpleoreexpansion/CreativeTab.java
@@ -1,5 +1,6 @@
 package com.dorasuzublog.simpleoreexpansion;
 
+import com.dorasuzublog.simpleoreexpansion.regist.Blocks;
 import com.dorasuzublog.simpleoreexpansion.regist.Items;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
@@ -19,6 +20,7 @@ public class CreativeTab {
         // アイテム一覧
         .displayItems((params, output) -> {
             output.accept(Items.EXAMPLE_ITEM.get());
+            output.accept(Blocks.EXAMPLE_BLOCK.get());
             
         })
         .build()
