df68997b27a4feeb529f2f079077fe3c1a332e49
どらすず <dorasuzu58@gmail.com>
2024-01-07 13:35:58 +0900

    Update Tags

--- src/main/java/com/dorasuzublog/simpleoreexpansion/regist/Tags.java
+++ src/main/java/com/dorasuzublog/simpleoreexpansion/regist/Tags.java
@@ -1,5 +1,27 @@
-package com.dorasuzublog.simpleoreexpansion;
+package com.dorasuzublog.simpleoreexpansion.regist;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 
 public class Tags {
-    
+    public static class Blocks {
+        //以下にタグを追加
+
+
+        private static TagKey<Block> tag (String name) {
+            return BlockTags.create(new ResourceLocation("simpleoreexpansion", name));
+        }
+    }
+    public static class Items {
+        //以下にタグを追加
+
+
+        private static TagKey<Item> tag (String name) {
+            return ItemTags.create(new ResourceLocation("simpleoreexpansion", name));
+        }
+    }
 }
